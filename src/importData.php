17fac60b41fd2059bf9a888dc48c4604d66491ee
peter <peter@huizendebeer.nl>
2018-01-09 22:29:49 +0100

    first commit

---
+++ src/importData.php
@@ -0,0 +1,7 @@
+<?php
+namespace Dumpert\ImportData;
+
+class importData
+{
+
+}
---
+++ tests/importUsersTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace Dumpert\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class importUsersTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function foo()
+    {
+        $this->assertTrue(true);
+    }
+
+}
